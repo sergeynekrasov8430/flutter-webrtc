20ba33bce2679274f02e857954ad3ca551707a90
VladimirBrejcha <vladimirbrejcha@icloud.com>
2021-08-01 21:56:43 +0300

    Add fix for huawei (see: https://github.com/sergeynekrasov8430/domsip_release/commit/14a1d0aefa2726b30d7122a6ef5ba94a53317d53)

--- android/src/main/java/com/cloudwebrtc/webrtc/MethodCallHandlerImpl.java
+++ android/src/main/java/com/cloudwebrtc/webrtc/MethodCallHandlerImpl.java
@@ -21,8 +21,6 @@ import com.cloudwebrtc.webrtc.utils.ObjectType;
 
 import org.webrtc.AudioTrack;
 import org.webrtc.CryptoOptions;
-import org.webrtc.DefaultVideoDecoderFactory;
-import org.webrtc.DefaultVideoEncoderFactory;
 import org.webrtc.DtmfSender;
 import org.webrtc.EglBase;
 import org.webrtc.IceCandidate;
